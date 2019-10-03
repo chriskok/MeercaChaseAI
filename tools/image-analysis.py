c22a8399ac70291779ff63cf2f59f414e4780ca2
Christopher Kok <ckok@purdue.edu>
2019-10-03 18:36:17 -0500

    Added image analysis for pathfinding

--- tools/image-analysis.py
+++ tools/image-analysis.py
@@ -1,16 +1,8 @@
-
 from PIL import ImageGrab, Image
 import numpy as np
 import cv2
 import time 
-import pyautogui
 import matplotlib.pyplot as plt
-
-# Full Screen
-X_START = 460
-Y_START = 180
-X_STOP = 1060
-Y_STOP = 780
 
 class Node():
     """A node class for A* Pathfinding"""
@@ -68,7 +60,7 @@ def astar(maze, start, end):
 
         # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
 
             # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
@@ -110,29 +102,22 @@ def astar(maze, start, end):
 
 # Returns a direction to press the button toward
 # 1 = up, 2 = right, 3 = down, 4 = left
-LAST_COMMAND = 0
 def getDirection(meer, goal):
-    global LAST_COMMAND
-    if (meer[1] - goal[1] < 0 and LAST_COMMAND != 2):
-        LAST_COMMAND = 2
+    if (meer[1] - goal[1] < 0):
         return 2
-    elif (meer[1] - goal[1] > 0 and LAST_COMMAND != 4):
-        LAST_COMMAND = 4
+    elif (meer[1] - goal[1] > 0):
         return 4
     
-    if (meer[0] - goal[0] > 0 and LAST_COMMAND != 1):
-        LAST_COMMAND = 1
+    if (meer[0] - goal[0] > 0):
         return 1
-    elif (meer[0] - goal[0] < 0 and LAST_COMMAND != 3):
-        LAST_COMMAND = 3
+    elif (meer[0] - goal[0] < 0):
         return 3
 
-    if (LAST_COMMAND == 1 or LAST_COMMAND == 3):
-        return 2
-    else:
-        return 1
+def main():
 
-def pathFindInImage(im_rgb):
+    image = cv2.imread('C:\\Users\\Christopher Kok\\Desktop\\MeercaChaseAI\\imgs\\new.png')
+    im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     myMap = []
 
     for row in im_rgb:
@@ -153,17 +138,15 @@ def pathFindInImage(im_rgb):
 
     dst = cv2.inRange(np.array(im_rgb), YELLOW_MIN, YELLOW_MAX)
     locations_of_color = np.transpose(dst.nonzero())
-    if (len(locations_of_color) == 0): return "NO GOAL"
     
     GOAL_X, GOAL_Y = locations_of_color[0]
 
     # FIND THE MEERCA
     MEER_MIN = np.array([200, 150, 0], np.uint8)
-    MEER_MAX = np.array([255, 200, 60], np.uint8)
+    MEER_MAX = np.array([255, 190, 50], np.uint8)
 
     dst2 = cv2.inRange(np.array(im_rgb), MEER_MIN, MEER_MAX)
     locations_of_meer = np.transpose(dst2.nonzero())
-    if (len(locations_of_meer) == 0): return "NO MEER"
     
     MEER_X, MEER_Y = locations_of_meer[0]
 
@@ -195,63 +178,15 @@ def pathFindInImage(im_rgb):
 
     # SHOW THE RESULTS
     # plt.imshow(myMap)
-    plt.imsave('imgs/state.png', myMap)
+    # plt.show()
 
-    dir = 0
     if (len(path) > spread):
         dir = getDirection(path[0], path[spread])
-    else:
-        dir = getDirection(path[0], path[1])
+        print(path[0])
+        print(path[spread])
+        if (dir == 1): print("up")
+        if (dir == 2): print("right")
+        if (dir == 3): print("down")
+        if (dir == 4): print("left")
 
-    if (dir == 1): 
-        pyautogui.keyDown("up")
-        pyautogui.keyUp("up")
-        print("up")
-    elif (dir == 2): 
-        pyautogui.keyDown("right")
-        pyautogui.keyUp("right")
-        print("right")
-    elif (dir == 3): 
-        pyautogui.keyDown("down")
-        pyautogui.keyUp("down")
-        print("down")
-    elif (dir == 4): 
-        pyautogui.keyDown('left')
-        pyautogui.keyUp("left")
-        print("left")
-    
-    return "OK"
-        
-def main():
-
-    for i in range(4):
-        print(i)
-        time.sleep(0.5)
-
-    # BLUE_MIN = np.array([0, 0, 0], np.uint8)
-    # BLUE_MAX = np.array([255, 0, 0], np.uint8)
-
-    # dst = cv2.inRange(np.array(new_im), BLUE_MIN, BLUE_MAX)
-    # no_red = cv2.countNonZero(dst)
-    # locations_of_red = np.transpose(dst.nonzero())
-
-    # if (no_red > 10 and no_red < 20):
-    #     im.save("imgs/current.png")
-    #     new_im.save("imgs/new.png",optimize=True,quality=95)
-
-    # print('The number of red pixels is: ' + str(no_red))
-
-    while True:
-        # Save part of the screen where game is
-        im=ImageGrab.grab(bbox=(X_START,Y_START,X_STOP,Y_STOP)).convert('RGB')
-        new_im = im.resize((100,100),Image.ANTIALIAS)
-
-        val = pathFindInImage(np.array(new_im))
-        if (val != "OK"): 
-            print(val)
-            break
-
-        time.sleep(0.5)
-
-if __name__ == '__main__':
-    main()
+main()
