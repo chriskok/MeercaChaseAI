660003f2ebd93ed3deb6c46aabb1eed2fa24f744
Christopher Kok <ckok@purdue.edu>
2019-10-03 21:50:17 -0500

    meerca test now works with windows keys

--- meerca-test.py
+++ meerca-test.py
@@ -1,16 +1,20 @@
-
 from PIL import ImageGrab, Image
 import numpy as np
 import cv2
 import time 
 import pyautogui
 import matplotlib.pyplot as plt
+from ahk import AHK
+
+ahk = AHK(executable_path='C:\\Program Files\\AutoHotkey\\AutoHotkey.exe')
 
 # Full Screen
 X_START = 460
-Y_START = 180
+Y_START = 140
 X_STOP = 1060
-Y_STOP = 780
+Y_STOP = 840
+COMPRESSION_SIZE = 100
+SPREAD = 3
 
 class Node():
     """A node class for A* Pathfinding"""
@@ -25,6 +29,9 @@ class Node():
 
     def __eq__(self, other):
         return self.position == other.position
+    
+    def __hash__(self):            
+        return hash(self.position)
 
 def astar(maze, start, end):
     """Returns a list of tuples as a path from the given start to the given end in the given maze"""
@@ -37,7 +44,7 @@ def astar(maze, start, end):
 
     # Initialize both open and closed list
     open_list = []
-    closed_list = []
+    closed_list = set()
 
     # Add the start node
     open_list.append(start_node)
@@ -55,7 +62,7 @@ def astar(maze, start, end):
 
         # Pop current off open list, add to closed list
         open_list.pop(current_index)
-        closed_list.append(current_node)
+        closed_list.add(current_node)
 
         # Found the goal
         if current_node == end_node:
@@ -91,9 +98,8 @@ def astar(maze, start, end):
         for child in children:
 
             # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
+            if child in closed_list:
+                continue
 
             # Create the f, g, and h values
             child.g = current_node.g + 1
@@ -107,6 +113,8 @@ def astar(maze, start, end):
 
             # Add the child to the open list
             open_list.append(child)
+
+
 
 # Returns a direction to press the button toward
 # 1 = up, 2 = right, 3 = down, 4 = left
@@ -128,28 +136,28 @@ def getDirection(meer, goal):
         return 3
 
     if (LAST_COMMAND == 1 or LAST_COMMAND == 3):
+        LAST_COMMAND = 2
         return 2
     else:
+        LAST_COMMAND = 1
         return 1
 
 def pathFindInImage(im_rgb):
+    global SPREAD
     myMap = []
 
     for row in im_rgb:
         currentRow = []
         for item in row:
             if (item == [153,153,153]).all():
-                # print(0, end="")
                 currentRow.append(0)
             else:
-                # print(item, end="")
                 currentRow.append(1)
         myMap.append(currentRow)
-        # print('\n')
 
     # FIND THE GOAL
     YELLOW_MIN = np.array([200, 200, 0], np.uint8)
-    YELLOW_MAX = np.array([255, 255, 50], np.uint8)
+    YELLOW_MAX = np.array([255, 255, 25], np.uint8)
 
     dst = cv2.inRange(np.array(im_rgb), YELLOW_MIN, YELLOW_MAX)
     locations_of_color = np.transpose(dst.nonzero())
@@ -158,8 +166,8 @@ def pathFindInImage(im_rgb):
     GOAL_X, GOAL_Y = locations_of_color[0]
 
     # FIND THE MEERCA
-    MEER_MIN = np.array([200, 150, 0], np.uint8)
-    MEER_MAX = np.array([255, 200, 60], np.uint8)
+    MEER_MIN = np.array([170, 130, 50], np.uint8)
+    MEER_MAX = np.array([210, 180, 100], np.uint8)
 
     dst2 = cv2.inRange(np.array(im_rgb), MEER_MIN, MEER_MAX)
     locations_of_meer = np.transpose(dst2.nonzero())
@@ -168,90 +176,82 @@ def pathFindInImage(im_rgb):
     MEER_X, MEER_Y = locations_of_meer[0]
 
     # DIG HOLES SO WE CAN PATHFIND BETWEEN THEM
-    spread = 6
-    for i in range (spread):
-        if GOAL_X + i < 100:
+    for i in range (SPREAD):
+        if GOAL_X + i < COMPRESSION_SIZE:
             myMap[GOAL_X + i][GOAL_Y] = 0
         if GOAL_X - i >= 0:
             myMap[GOAL_X - i][GOAL_Y] = 0
-        if GOAL_Y + i < 100:
+        if GOAL_Y + i < COMPRESSION_SIZE:
             myMap[GOAL_X][GOAL_Y + i] = 0
         if GOAL_Y - i >= 0:
             myMap[GOAL_X][GOAL_Y - i] = 0
 
-        if MEER_X + i < 100:
+        if MEER_X + i < COMPRESSION_SIZE:
             myMap[MEER_X + i][MEER_Y] = 0
         if MEER_X - i >= 0:
             myMap[MEER_X - i][MEER_Y] = 0
-        if MEER_Y + i < 100:
+        if MEER_Y + i < COMPRESSION_SIZE:
             myMap[MEER_X][MEER_Y + i] = 0
         if MEER_Y - i >= 0:
             myMap[MEER_X][MEER_Y - i] = 0
 
+
     # PATHFIND
     path = astar(myMap, (MEER_X,MEER_Y), (GOAL_X, GOAL_Y))
-    for item in path:
-        myMap[item[0]][item[1]] = 2
 
     # SHOW THE RESULTS
-    # plt.imshow(myMap)
+    for item in path:
+        myMap[item[0]][item[1]] = 2
     plt.imsave('imgs/state.png', myMap)
 
     dir = 0
-    if (len(path) > spread):
-        dir = getDirection(path[0], path[spread])
+    if (len(path) > SPREAD * 2):
+        dir = getDirection(path[0], path[SPREAD * 2])
+    elif (len(path) > SPREAD): 
+        dir = getDirection(path[0], path[SPREAD])
     else:
         dir = getDirection(path[0], path[1])
 
     if (dir == 1): 
-        pyautogui.keyDown("up")
-        pyautogui.keyUp("up")
+        # pyautogui.press("up")
+        ahk.key_press('up')
         print("up")
     elif (dir == 2): 
-        pyautogui.keyDown("right")
-        pyautogui.keyUp("right")
+        # pyautogui.press("right")
+        ahk.key_press('right')
         print("right")
     elif (dir == 3): 
-        pyautogui.keyDown("down")
-        pyautogui.keyUp("down")
+        # pyautogui.press("down")
+        ahk.key_press('down')
         print("down")
     elif (dir == 4): 
-        pyautogui.keyDown('left')
-        pyautogui.keyUp("left")
+        # pyautogui.press('left')
+        ahk.key_press('left')
         print("left")
     
     return "OK"
         
 def main():
 
-    for i in range(4):
+    for i in range(3):
         print(i)
         time.sleep(0.5)
 
-    # BLUE_MIN = np.array([0, 0, 0], np.uint8)
-    # BLUE_MAX = np.array([255, 0, 0], np.uint8)
-
-    # dst = cv2.inRange(np.array(new_im), BLUE_MIN, BLUE_MAX)
-    # no_red = cv2.countNonZero(dst)
-    # locations_of_red = np.transpose(dst.nonzero())
-
-    # if (no_red > 10 and no_red < 20):
-    #     im.save("imgs/current.png")
-    #     new_im.save("imgs/new.png",optimize=True,quality=95)
-
-    # print('The number of red pixels is: ' + str(no_red))
-
+    counter = 0
     while True:
         # Save part of the screen where game is
         im=ImageGrab.grab(bbox=(X_START,Y_START,X_STOP,Y_STOP)).convert('RGB')
-        new_im = im.resize((100,100),Image.ANTIALIAS)
+        new_im = im.resize((COMPRESSION_SIZE,COMPRESSION_SIZE),Image.ANTIALIAS)
+
+        # new_im.save("imgs/new{}.png".format(counter),optimize=True,quality=95)
 
         val = pathFindInImage(np.array(new_im))
         if (val != "OK"): 
             print(val)
-            break
 
-        time.sleep(0.5)
+        # time.sleep(1)
+
+        # counter += 1
 
 if __name__ == '__main__':
     main()
